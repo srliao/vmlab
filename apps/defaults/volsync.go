d2a697edf0a21189849d14958c8d4bc0e398099d
srliao <906239+srliao@users.noreply.github.com>
2025-09-24 13:44:30 -0400

    update secrets

--- apps/defaults/volsync.go
+++ apps/defaults/volsync.go
@@ -85,7 +85,7 @@ func newVolsyncES(app string) klusterhelper.KubeResource {
 			"AWS_ACCESS_KEY_ID":     "{{ .AWS_ACCESS_KEY_ID }}",
 			"AWS_SECRET_ACCESS_KEY": "{{ .AWS_SECRET_ACCESS_KEY }}",
 		},
-		"obsidian",
+		"volsync",
 	)
 }
 
