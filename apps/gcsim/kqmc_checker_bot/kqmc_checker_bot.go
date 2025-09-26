c0f8efc7474691f40f2d3159d1e8f287de0f5d75
srliao <906239+srliao@users.noreply.github.com>
2025-09-26 14:17:00 -0400

    add kqmc-checker-bot

---
+++ apps/gcsim/kqmc_checker_bot/kqmc_checker_bot.go
@@ -0,0 +1,77 @@
+package kqmc_checker_bot
+
+import (
+	"log"
+
+	_ "embed"
+
+	fluxmeta "github.com/fluxcd/pkg/apis/meta"
+	"github.com/srliao/vmlab/apps/defaults"
+	"github.com/srliao/vmlab/pkg/klusterhelper"
+)
+
+const (
+	name          = "kqmc-checker-bot"
+	namespace     = "gcsim"
+	component     = "gcsim"
+	discordSecret = "kqmc-checker-bot-secrets"
+)
+
+var imageSpec = &klusterhelper.ImageSpec{
+	Repository: "ghcr.io/charlie-zheng/gcsim-kqmc-checker-develop",
+	Tag:        "rolling@sha256:b3d13f9d0d54f7fabed3b62d36847f3f1585b3ba1f0f04be6e8ea5d8587c10fd",
+}
+
+func Chart() *klusterhelper.App {
+	subpath, err := defaults.RelDirPath(1)
+	if err != nil {
+		log.Panicf("getting subpath failed for %v/%v: %v", name, namespace, err)
+	}
+
+	ks := defaults.NewFluxKS(name, namespace, subpath)
+	ks.WithDependsOn([]fluxmeta.NamespacedObjectReference{
+		{
+			Name: "external-secrets-stores",
+		},
+	})
+
+	c := klusterhelper.
+		NewApp(name, namespace, subpath).
+		SetKS(ks).
+		AddObjects(
+			deployment(),
+			secrets(),
+		)
+
+	return c
+}
+
+func deployment() klusterhelper.KubeResource {
+
+	// app container
+	container := defaults.
+		NewContainer(name, imageSpec).
+		WithCPURequest("20m").
+		WithMemoryRequest("512Mi").
+		WithMemoryLimit("1024Mi").
+		AddEnvFromSecret(discordSecret)
+
+	deploy := defaults.
+		NewDeployment(name, namespace)
+
+	deploy.PodTemplate().
+		AddContainer(container)
+
+	return deploy
+}
+
+func secrets() klusterhelper.KubeResource {
+	return defaults.NewESWithDataAndKey(
+		name,
+		discordSecret,
+		map[string]string{
+			"DISCORD_TOKEN": "{{ .KQMC_CHECKER_BOT_DISCORD_TOKEN }}",
+		},
+		"gcsim",
+	)
+}
--- apps/main.go
+++ apps/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/srliao/vmlab/apps/database/couchdb_obsidian"
+	"github.com/srliao/vmlab/apps/gcsim/kqmc_checker_bot"
 	"github.com/srliao/vmlab/pkg/klusterhelper"
 )
 
 func main() {
 	klusterhelper.NewBuilder().
 		AddApp(couchdb_obsidian.Chart()).
+		AddApp(kqmc_checker_bot.Chart()).
 		Validate().
 		Build("../kubernetes/apps", true)
 }
